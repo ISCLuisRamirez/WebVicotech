cf7308b90d088bae9b4b1bdde6630ed804b549f0
ISCLuisRamirez <isc.luisramirez@gmail.com>
2024-05-14 17:59:39 -0600

    link store added

--- index.php
+++ index.php
@@ -52,9 +52,9 @@
           <li><a class="nav-link scrollto active" href="#hero">Inicio</a></li>
           <li><a class="nav-link scrollto" href="#about">Empresa</a></li>
           <li><a class="nav-link scrollto" href="#services">Servicios</a></li>
-          <li><a class="nav-link   scrollto" href="#portfolio">Portafolio</a></li>
-          <!-- <li><a class="nav-link scrollto" href="#team">Team</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+          <li><a class="nav-link scrollto" href="#portfolio">Portafolio</a></li>
+          <li><a class="nav-link scrollto" href="https://vicotechmx.mercadoshops.com.mx" target="_blank">Tienda</a></li>
+          <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
               <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
